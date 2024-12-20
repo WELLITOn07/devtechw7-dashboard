7dacbfe9b47b10b25f822f68ac74e77aee5c6cd7
WELLITOn07 <w.elliton2010@hotmail.com>
2024-12-20 15:38:30 -0300

    fix: add lazy load

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,33 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import LoginView from "@/views/LoginView.vue";
-import ManageControllersView from "@/views/ManageControllersView.vue";
-import AnalyticsDashboard from "@/views/AnalyticsDashboard.vue";
-import AdvertisementCentralView from "@/views/AdvertisementCentralView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: () => import("@/views/HomeView.vue"),
     meta: { requiresAuth: true },
   },
   {
     path: "/login",
     name: "login",
-    component: LoginView,
+    component: () => import("@/views/LoginView.vue"),
   },
   {
     path: "/manage/:name",
     name: "manage-controllers",
-    component: ManageControllersView,
+    component: () => import("@/views/ManageControllersView.vue"),
     meta: { requiresAuth: true },
   },
   {
     path: "/analytics",
     name: "analytics-dashboard",
-    component: AnalyticsDashboard,
+    component: () => import("@/views/AnalyticsDashboard.vue"),
     meta: { requiresAuth: true },
   },
   {
     path: "/advertisements",
     name: "advertisement-central",
-    component: AdvertisementCentralView,
+    component: () => import("@/views/AdvertisementCentralView.vue"),
     meta: { requiresAuth: true },
   },
 ];
